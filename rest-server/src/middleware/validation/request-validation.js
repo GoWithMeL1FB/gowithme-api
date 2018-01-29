2af99d16083df0dde399153364d98640ce4a501f
David Yonwook Chung <yon_gospel@hotmail.com>
2018-01-29 15:54:33 -0800

    [auth] adds joi validation for login using username

--- rest-server/src/middleware/validation/request-validation.js
+++ rest-server/src/middleware/validation/request-validation.js
@@ -10,5 +10,11 @@ export default {
       password: Joi.string().regex(/^[a-zA-Z0-9]{8,15}$/).required(),
       birthyear: Joi.number().integer().min(currentYear - 13).max(currentYear).required()
     }
+  },
+  login: {
+    body: {
+      username: Joi.string().regex(/^[a-zA-Z0-9]{5,15}$/).required(), //username has to be string, mixtures of alphabets and numbers, length betwee 5 to 15
+      password: Joi.string().regex(/^[a-zA-Z0-9]{8,15}$/).required()
+    }
   }
 }
