75b90a7667851019da6cbfdd6694004887b0e71b
Kevin Vo <kevinvoduy218@gmail.com>
2018-01-29 11:43:48 -0800

    [add] - error highlighting with chalk

---
+++ rest-server/lib/logger/index.js
@@ -0,0 +1,25 @@
+import * as chalk from 'chalk';
+
+export const success = (...logger) => {
+  if (process.env.DEBUG === 'TRUE') {
+    console.log(chalk.default.white.bgGreen.bold(...logger));
+  }
+};
+
+export const warning = (...log) => {
+  if (process.env.DEBUG === 'TRUE') {
+    console.log(chalk.default.white.bgYellow.bold(...logger));
+  }
+};
+
+export const error = (...logger) => {
+  if (process.env.DEBUG === 'TRUE') {
+    console.log(chalk.default.white.bgRed.bold(...logger));
+  }
+};
+
+export const log = (...logger) => {
+  if (process.env.DEBUG === 'TRUE') {
+    console.log(chalk.default.white.bdWhite.bold(...logger));
+  }
+};
