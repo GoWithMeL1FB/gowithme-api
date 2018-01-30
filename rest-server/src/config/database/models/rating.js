0361c35ed1fafda5d9f28c776948ac2d166c5482
Kevin Vo <kevinvoduy218@gmail.com>
2018-01-30 13:12:18 -0800

    [edit] - synced tables show logs / messing with travis

--- rest-server/src/config/database/models/rating.js
+++ rest-server/src/config/database/models/rating.js
@@ -1,14 +1,14 @@
 const Sequelize = require("sequelize");
 const db = require('../index');
 
-module.exports = db.define( 'rating', {
+module.exports = db.define('rating', {
   id: {
     type: Sequelize.INTEGER,
     primaryKey: true,
     autoIncrement: true
-    },
+  },
 	user_id: {
-    type: Sequelize.INTEGER
+    type: Sequelize.INTEGER,
   },
 	dateCourse: {
     type: Sequelize.INTEGER
--- rest-server/src/config/database/models/sync.js
+++ rest-server/src/config/database/models/sync.js
@@ -1,9 +1,9 @@
 const Users = require('./users');
 const categories = require('./categories');
-//const dateCourse = require('./dateCourse');
-//const events = require('./events');
+const dateCourse = require('./dateCourse');
+const events = require('./events');
 const rating = require('./rating');
-//const dataCourseEvents = require('./dateCourseEvents');
+const dataCourseEvents = require('./dateCourseEvents');
 const comments = require('./comments');
 const categoryJoin = require('./categoryJoin');
 import { success, error } from '../../../lib/logger';
@@ -11,17 +11,62 @@ import { success, error } from '../../../lib/logger';
 
 //assign dependencies
 //try without asyc
-Users
-.sync()
-.then( () => {
-  categories.sync();
-})
-.then( () => {
-  rating.sync();
-  comments.sync();
-  categoryJoin.sync();
-
-});
+Users.sync()
+  .then( () => {
+    success('USERS table synced')
+    categories.sync()
+      .then(() => {
+        success('CATEGORIES table synced')
+        comments.sync()
+          .then(() => {
+            success('COMMENTS table synced')
+            categoryJoin.sync()
+              .then(() => {
+                success('CATEGORYJOIN table synced')
+                dateCourse.sync()
+                  .then(() => {
+                    success('DATECOURSE table synced')
+                    events.sync()
+                      .then(() => {
+                        success('EVENTS table synced')
+                        rating.sync()
+                          .then(() => {
+                            success('RATING table synced')
+                            dataCourseEvents.sync()
+                              .then(() => {
+                                success('DC-EVENTS table synced')
+                              })
+                              .catch(() => {
+                                error('dateCourseEvents table failed to sync')
+                              })
+                          })
+                          .catch(() => {
+                            error('rating table failed to sync')
+                          })
+                      })
+                      .catch(() => {
+                        error('events table failed to sync')
+                      })
+                  })
+                  .catch(() => {
+                    error('dateCourse table failed to sync')
+                  })
+              })
+              .catch(() => {
+                error('categoryJoin table failed to sync')
+              })
+          })
+          .catch(() => {
+            error('comments table failed to sync')
+          })
+      })
+      .catch(() => {
+        error('categories table failed to sync')
+      })
+  })
+  .catch((err) => {
+    error('failed to syn users table: ', err)}
+  )
 
 module.exports = { Users, categories, rating, comments, categoryJoin };
 //promisafiy sync tables
