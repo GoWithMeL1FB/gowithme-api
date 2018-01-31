54c5f8e064ea6baa810b3e276ff70f7b209855fc
David Yonwook Chung <yon_gospel@hotmail.com>
2018-01-30 16:08:19 -0800

    [auth] implements fb and google passports to our app

--- rest-server/src/components/auth/authRouter.js
+++ rest-server/src/components/auth/authRouter.js
@@ -6,6 +6,10 @@ import { signUpController, loginController } from './authController';
 import formValidation from '../../middleware/validation/request-validation';
 import '../../middleware/validation/passport';
 
+const passportFacebook = require('../../middleware/validation/socialApps/facebook.js')
+const passportGoogle = require('../../middleware/validation/socialApps/google.js')
+
+
 const router = express.Router();
 
 router.route('/signup')
@@ -13,5 +17,6 @@ router.route('/signup')
 
 router.route('/login')
   .post(validate(formValidation.login), passport.authenticate('local', { session: false}), loginController);
+
 
 export default router;
