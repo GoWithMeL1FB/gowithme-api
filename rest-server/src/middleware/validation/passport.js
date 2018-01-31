6611ac88bd2446b2cf715c8e3353180703260522
David Yonwook Chung <yon_gospel@hotmail.com>
2018-01-30 17:33:45 -0800

    [auth] initializes passport on app.express()

--- rest-server/src/middleware/validation/passport.js
+++ rest-server/src/middleware/validation/passport.js
@@ -2,4 +2,18 @@ import passport from 'passport';
 import localPP from 'passport-local';
 import jwtPP from 'passport-jwt';
 import fbpp from 'passport-facebook';
-import googlepp from 'passport-google-auth';
+import googlepp from 'passport-google-oauth';
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
+
+module.exports = passport;
--- rest-server/src/middleware/validation/socialApps/google.js
+++ rest-server/src/middleware/validation/socialApps/google.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const User = require('../models/user');
+// const User = require('../models/user');
 const GOOGLE_ID = process.env.GOOGLE_ID;
 const GOOGLE_SECRET = process.env.GOOGLE_SECRET;
 
@@ -8,7 +8,7 @@ const GOOGLE_SECRET = process.env.GOOGLE_SECRET;
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_ID,
     clientSecret: GOOGLE_SECRET,
-    callbackURL: "http://127.0.0.1:3050/auth/google/callback"
+    callbackURL: "http://127.0.0.1:3050/api/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
        User.findOrCreate({ userid: profile.id }, { name: profile.displayName,userid: profile.id }, function (err, user) {
