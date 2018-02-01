bb45bdeb2c4909a0815385c08fe32c8ee79dfaeb
Kevin Vo <kevinvoduy218@gmail.com>
2018-01-31 22:52:05 -0800

    [ops]

--- rest-server/src/config/database/models/index.js
+++ rest-server/src/config/database/models/index.js
@@ -11,72 +11,72 @@ import { success, error, warning } from '../../../lib/logger';
 
 //assign dependencies
 //try without asyc
-Users.sync()
-  .then( () => {
-    categories.sync()
-      .then(() => {
-        comments.sync()
-          .then(() => {
-            categoryJoin.sync()
-              .then(() => {
-                dateCourse.sync()
-                  .then(() => {
-                    events.sync()
-                      .then(() => {
-                        rating.sync()
-                          .then(() => {
-                            dateCourseEvents.sync()
-                              .then(() => {
-                                success('tables - all synced')
-                              })
-                              .catch(() => {
-                                error('dateCourseEvents table failed to sync')
-                              })
-                          })
-                          .catch(() => {
-                            error('rating table failed to sync')
-                          })
-                      })
-                      .catch(() => {
-                        error('events table failed to sync')
-                      })
-                  })
-                  .catch(() => {
-                    error('dateCourse table failed to sync')
-                  })
-              })
-              .catch(() => {
-                error('categoryJoin table failed to sync')
-              })
-          })
-          .catch(() => {
-            error('comments table failed to sync')
-          })
-      })
-      .catch(() => {
-        error('categories table failed to sync')
-      })
-  })
-  .catch((err) => {
-    error('failed to syn users table: ', err)}
-  )
+// Users.sync()
+//   .then( () => {
+//     categories.sync()
+//       .then(() => {
+//         comments.sync()
+//           .then(() => {
+//             categoryJoin.sync()
+//               .then(() => {
+//                 dateCourse.sync()
+//                   .then(() => {
+//                     events.sync()
+//                       .then(() => {
+//                         rating.sync()
+//                           .then(() => {
+//                             dateCourseEvents.sync()
+//                               .then(() => {
+//                                 success('tables - all synced')
+//                               })
+//                               .catch(() => {
+//                                 error('dateCourseEvents table failed to sync')
+//                               })
+//                           })
+//                           .catch(() => {
+//                             error('rating table failed to sync')
+//                           })
+//                       })
+//                       .catch(() => {
+//                         error('events table failed to sync')
+//                       })
+//                   })
+//                   .catch(() => {
+//                     error('dateCourse table failed to sync')
+//                   })
+//               })
+//               .catch(() => {
+//                 error('categoryJoin table failed to sync')
+//               })
+//           })
+//           .catch(() => {
+//             error('comments table failed to sync')
+//           })
+//       })
+//       .catch(() => {
+//         error('categories table failed to sync')
+//       })
+//   })
+//   .catch((err) => {
+//     error('failed to syn users table: ', err)}
+//   )
 
 module.exports = { Users, categories, rating, comments, categoryJoin };
 //promisafiy sync tables
-// const syncTables = async () => {
-//   try {
-//     await users.sync();
-//    // await dataCourse.sync();
-//     await categories.sync();
-//    // await events.sync();
-//     await rating.sync();
-//    // await dataCourseEvents.sync();
-//     await comments.sync();
-//     await categoryJoin.sync();
-//     await seedTables();
-//     success('successfully created users table ');
-//   } catch (err) {
-//     error('error creating database ', err);
-//   }
-// };
-// syncTables()
+const syncTables = async () => {
+  try {
+    await users.sync();
+   // await dataCourse.sync();
+    await categories.sync();
+   // await events.sync();
+    await rating.sync();
+   // await dataCourseEvents.sync();
+    await comments.sync();
+    await categoryJoin.sync();
+    await seedTables();
+    success('successfully created users table ');
+  } catch (err) {
+    error('error creating database ', err);
+  }
+};
+syncTables()
