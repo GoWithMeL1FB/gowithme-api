83d7484811c665adb061d28b898e5356df6ca7db
Kevin Vo <kevinvoduy218@gmail.com>
2018-01-31 23:14:29 -0800

    [edit] - added username and pw to creat db

--- rest-server/src/config/database/index.js
+++ rest-server/src/config/database/index.js
@@ -4,8 +4,7 @@ import Sequelize from 'sequelize';
 import Promise from 'bluebird';
 // const seedTables = require('./models/seed');
 
-const db = new Sequelize('GoWithMe', 'root', '', {
-
+const db = new Sequelize('GoWithMe', 'gowithme', 'password', {
   host: 'localhost',
   dialect: 'mysql',
    port: 3306,
