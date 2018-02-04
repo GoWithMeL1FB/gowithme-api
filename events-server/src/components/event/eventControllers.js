67b8f3480e8eb5a30800383b58a0974686c40ea2
Kevin Vo <kevinvoduy218@gmail.com>
2018-02-03 20:29:17 -0800

    [events] - creates itinerary with out any events

--- events-server/src/components/event/eventControllers.js
+++ events-server/src/components/event/eventControllers.js
@@ -1,4 +1,6 @@
 import { createEventQuery } from './eventQueries';
+import Itinerary from '../../config/schemas/itinerary';
+// import Events from '../../config/schemas/event';
 import { success, error } from '../../lib/logger';
 
 export const createEventController = async (req, res) => {
@@ -8,6 +10,26 @@ export const createEventController = async (req, res) => {
     return res.status(200).send(result);
   } catch (err) {
     error('event was not created', err);
+    res.status(403).send(err.message);
+  }
+};
+
+export const createItinerary = async (req, res) => {
+  try {
+    const { title, owner } = req.body;
+
+    // new instance of Itinearry with props from req.body
+    const newItinerary = new Itinerary({
+      title,
+      owner,
+    });
+
+    // saves itinerary instance to db
+    newItinerary.save();
+    success('Controller - new itinerary');
+    return res.status(200).send(newItinerary);
+  } catch (err) {
+    error('itinerary was not crated', err);
     res.status(403).send(err.message);
   }
 };
