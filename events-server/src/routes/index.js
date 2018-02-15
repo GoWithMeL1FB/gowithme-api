d5b6eb605dd18564d4e1dfe351fb8c6afc12da61
David Yonwook Chung <yon_gospel@hotmail.com>
2018-02-15 09:47:35 -0800

    [google] implements google search file structure

--- events-server/src/routes/index.js
+++ events-server/src/routes/index.js
@@ -4,6 +4,7 @@ import eventRouter from '../components/event/eventRouter';
 import itineraryRouter from '../components/itinerary/itineraryRouter';
 import favoritesRouter from '../components/favorites/favoritesRouter';
 import statsRouter from '../components/stats/statsRouter';
+import googleRouter from '../components/google/googleRouter';
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use('/events', eventRouter);
 router.use('/itinerary', itineraryRouter);
 router.use('/favorites', favoritesRouter);
 router.use('/stats', statsRouter);
+router.use('/google', googleRouter);
 
 export default router;
