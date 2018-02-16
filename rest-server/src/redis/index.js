6180b89b6f9d5e66e12f08c30a6c2a61a5adfc80
David Yonwook Chung <yon_gospel@hotmail.com>
2018-02-16 02:16:53 -0800

    [fix] redis halt

--- rest-server/src/redis/index.js
+++ rest-server/src/redis/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 import Promise from 'bluebird';
 import redis from 'redis';
@@ -5,9 +6,18 @@ const client = redis.createClient();
 // download server with brew install redis
 // start the server run: redis-server /usr/local/etc/redis.conf
 // defaults to 127.0.0.1 port 6379
+=======
+// import Promise from 'bluebird';
+// const redis = require("redis")
+// const client = redis.createClient();
+// // download server with brew install redis
+// // start the server run: redis-server /usr/local/etc/redis.conf
+// // defaults to 127.0.0.1 port 6379
+>>>>>>> [fix] redis halt
 
-Promise.promisifyAll(redis.RedisClient.prototype);
+// Promise.promisifyAll(redis.RedisClient.prototype);
 
+<<<<<<< HEAD
 client.on("connect", () => {
     console.log('redis connected!');
 });
@@ -29,22 +39,46 @@ export const getUserStats = async (username) => {
         console.log(err);
     }
 }
+=======
+// client.on("connect", () => {
+//     console.log('redis connected!');
+// });
+// client.on("error", function(err) {
+//     console.log("Redis Error", + err);
+//   });
 
-export const getUserImg = async (userImgRef) => {
-    try{
-        let img = null
-        await client.getAsync(`${userImgRef}`)
-        .then((reply) => {
-            img = reply;
-        }) 
-        console.log("******REDIS IMAGE", img);
-        return img
-    } catch(err) {
-        console.log(err);
-    }
+// export const getUserStats = async (username) => {
+//     try{
+//         let stats = null;
+//         console.log('heyhey', username);
+//         await client.hgetallAsync(`${username}`)
+//         .then((object) => {
+//             stats = object;
+//         }) 
+//         console.log("!!***** getUserStats function ", username, stats)
+//         return stats
+//     } catch(err) {
+//         console.log(err);
+//     }
+// }
+>>>>>>> [fix] redis halt
 
-}
+// export const getUserImg = async (userImgRef) => {
+//     try{
+//         let img = null
+//         await client.getAsync(`${userImgRef}`)
+//         .then((reply) => {
+//             img = reply;
+//         }) 
+//         console.log("******REDIS IMAGE", img);
+//         return img
+//     } catch(err) {
+//         console.log(err);
+//     }
 
+// }
+
+<<<<<<< HEAD
 export const setUserStats = (array) => {
     for(let i = 0; i < array.length; i++) {
         console.log('set user stats', array[i].username);
@@ -61,8 +95,25 @@ export const setUserStats = (array) => {
 
 
 export const setUserImg = (key, imgLink) => {
-    
-        client.set(`${key}`, `${imgLink}`, redis.print);
-        getUserImg(`${key}`);
+=======
+// export const setUserStats = (array) => {
+//     for(let i = 0; i < array.length; i++) {
+//         console.log('set user stats', array[i].username);
+//         client.hmset(`${array[i].username}`, {
+//             'dateCourseCount': `${array[i].count}`, 
+//             'mostSharedDC': `${array[i].mostShared.name}`, 
+//             'mostLikedDC': `${array[i].mostLiked.name}` })
+        
+//         client.hgetall(`${array[i].username}`, function(err, object) {
+//             console.log(object);
+//         });
+//     }
+// }
 
-}
+// export const setUserImg = (key, imgLink) => {
+>>>>>>> [fix] redis halt
+    
+//         client.set(`${key}`, `${imgLink}`, redis.print);
+//         getUserImg(`${key}`);
+
+// }
