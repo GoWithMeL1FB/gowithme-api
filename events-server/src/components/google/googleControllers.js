fc8a7d3140ff30a1eb16ce85f1b8c20141d628c0
David Yonwook Chung <yon_gospel@hotmail.com>
2018-02-15 17:05:43 -0800

    [rebase]

--- events-server/src/components/google/googleControllers.js
+++ events-server/src/components/google/googleControllers.js
@@ -1,10 +1,15 @@
 import gma from 'google-distance-matrix';
 const api = process.env.GOOGLEAPI;
 gma.key(api);
-
+gma.units('imperial');
 
 export const getDistance = (req, res) => {
-    gma.matrix(['40.6655101,-73.89188969999998'], ['34.108731021824674, -117.98608303070067'], function(err, distance) {
+    console.log('is the function getting invoked?????')
+    console.log('this is req.body', req.query)
+    const origin = req.query.origin;
+    const destination = req.query.destination;
+
+    gma.matrix(origin, destination, function(err, distance) {
       if (!err)
 
         console.log(distance);
